97a93122eadeaee11377b2d52453a3010cb7eec5
bierdok <2333728+bierdok@users.noreply.github.com>
2025-03-03 12:51:00 +0100

    Publish

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
-# TalkOps Extension: Boilerplate NodeJS
+# TalkOps Extension: OpenHAB
+![Docker Pulls](https://img.shields.io/docker/pulls/bierdok/talkops-extension-openhab)
 
 A TalkOps Extension made to work with [TalkOps](https://link.talkops.app/talkops).
 
-This Extension serves as a boilerplate designed to assist Node.js developers in effortlessly creating and integrating their own extensions.
+This Extension based on [OpenHAB](https://www.openhab.org/) allows you to **control your smart home by voice in realtime**.
+
+Features:
+* Switchs: Check status, turn on/off
+* Shutters: Check status, open, close and stop
 
 ## Installation Guide
 
 _[TalkOps](https://link.talkops.app/install-talkops) must be installed beforehand._
 
+* [Generate an API token](https://www.openhab.org/docs/configuration/apitokens.html#generate-an-api-token)
 
 ## Integration Guide
 
@@ -19,12 +25,24 @@ name: talkops
 
 services:
 ...
-  talkops-extension-boilerplate-nodejs:
-    image: ghcr.io/talkops/talkops-boilerplate-nodejs
+  talkops-extension-openhab:
+    image: bierdok/talkops-extension-openhab
+    environment:
+      BASE_URL: [your-value]
+      API_TOKEN: [your-value]
     restart: unless-stopped
 ```
 
 ## Environment Variables
+
+#### BASE_URL
+
+The base URL of your OpenHAB server.
+* Possible values: `http://openhab:8080` `https://openhab.mydomain.net`
+
+#### API_TOKEN
+
+The copied API token.
 
 #### AGENT_URLS
 
--- index.js
+++ index.js
@@ -1,41 +1,192 @@
 import { Extension, Readme, Service } from "talkops";
 
-const extension = new Extension("Boilerplate NodeJS");
+const extension = new Extension("OpenHAB");
 
-extension.setDockerRepository("ghcr.io/talkops/talkops-boilerplate-nodejs")
+extension.setDockerRepository("bierdok/talkops-extension-openhab");
 
 extension.setDescription(`
-This Extension serves as a boilerplate designed to assist Node.js developers in effortlessly creating and integrating their own extensions.
+This Extension based on [OpenHAB](https://www.openhab.org/) allows you to **control your smart home by voice in realtime**.
+
+Features:
+* Switchs: Check status, turn on/off
+* Shutters: Check status, open, close and stop
 `);
 
-extension.setInstructions(`
-You are an intelligent and efficient note-taking assistant.
-Your role is to help users capture, organize, and refine their notes effectively.
-Provide clear, concise, and structured responses while ensuring readability and usability.
-Adapt to the user's style and preferences to enhance their note-taking experience.
+extension.setInstallationGuide(`
+* [Generate an API token](https://www.openhab.org/docs/configuration/apitokens.html#generate-an-api-token)
 `);
 
-extension.setFunctionSchemas([
-  {
-    name: "save_note",
-    description: "Save a note after confirmation",
-    parameters: {
-      type: "object",
-      properties: {
-        note: {
-          type: "string",
-          description: "The related note",
-        },
-      },
-      required: ["note"],
-    },
+extension.setEnvironmentVariables({
+  BASE_URL: {
+    description: "The base URL of your OpenHAB server.",
+    possibleValues: ["http://openhab:8080", "https://openhab.mydomain.net"],
   },
-]);
+  API_TOKEN: {
+    description: "The copied API token.",
+  },
+});
+
+const baseInstructions = `
+You are a home automation assistant, focused solely on managing connected devices in the home.
+When asked to calculate an average, **round to the nearest whole number** without explaining the calculation.
+`;
+
+const defaultInstructions = `
+Currently, there is no connected devices.
+Your sole task is to ask the user to install one or more connected devices in the home before proceeding.
+`;
+
+import axios from "axios";
+import yaml from "js-yaml";
+
+import locationsModel from "./schemas/models/locations.json" assert { type: "json" };
+import switchsModel from "./schemas/models/switchs.json" assert { type: "json" };
+import shuttersModel from "./schemas/models/shutters.json" assert { type: "json" };
+
+import updateSwitchsFunction from "./schemas/functions/update_switchs.json" assert { type: "json" };
+import updateShuttersFunction from "./schemas/functions/update_shutters.json" assert { type: "json" };
+
+async function getSystemInfo() {
+  try {
+    const response = await axios.get(
+      `${process.env.BASE_URL}/rest/systeminfo`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.API_TOKEN}`,
+        },
+      }
+    );
+    return response.data.systemInfo;
+  } catch (err) {
+    extension.errors = [err.message];
+    return {};
+  }
+}
+
+async function getItems() {
+  try {
+    const response = await axios.get(`${process.env.BASE_URL}/rest/items`, {
+      headers: {
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
+      },
+    });
+    return response.data;
+  } catch (err) {
+    extension.errors = [err.message];
+    return [];
+  }
+}
+
+async function refresh() {
+  const locations = [];
+  const switchs = [];
+  const shutters = [];
+  extension.errors = [];
+  const systemInfo = await getSystemInfo();
+  extension.setVersion(systemInfo.osVersion);
+  for (const item of await getItems()) {
+    if (item.type === "Group" && item.tags.includes("Location")) {
+      locations.push({
+        id: item.name,
+        name: item.label,
+        location_id: item.groupNames.length ? item.groupNames[0] : null,
+      });
+    }
+    if (item.type === "Switch" && item.tags.includes("Equipment")) {
+      switchs.push({
+        id: item.name,
+        name: item.label,
+        state: item.state.toLowerCase(),
+        location_id: item.groupNames.length ? item.groupNames[0] : null,
+      });
+    }
+    if (item.type === "Rollershutter" && item.tags.includes("Equipment")) {
+      shutters.push({
+        id: item.name,
+        name: item.label,
+        state: item.state === "0" ? "opened" : "closed",
+        location_id: item.groupNames.length ? item.groupNames[0] : null,
+      });
+    }
+  }
+
+  extension.setInstructions(() => {
+    const instructions = [baseInstructions];
+
+    if (!switchs.length && !shutters.length) {
+      instructions.push(defaultInstructions);
+    } else {
+      instructions.push("``` yaml");
+      instructions.push(
+        yaml.dump({
+          locationsModel,
+          switchsModel,
+          shuttersModel,
+          locations,
+          switchs,
+          shutters,
+        })
+      );
+      instructions.push("```");
+    }
+
+    return instructions;
+  });
+
+  extension.setFunctionSchemas(() => {
+    const functionSchemas = [];
+    if (switchs.length) {
+      functionSchemas.push(updateSwitchsFunction);
+    }
+    if (shutters.length) {
+      functionSchemas.push(updateShuttersFunction);
+    }
+    return functionSchemas;
+  });
+
+  setTimeout(refresh, 5000);
+}
+refresh();
 
 extension.setFunctions([
-  function save_note(note) {
-    console.log(`Do something with: ${note}`);
-    return "Done.";
+  async function update_switchs(action, ids) {
+    try {
+      for (const id of ids) {
+        await axios.post(
+          `${process.env.BASE_URL}/rest/items/${id}`,
+          action.toUpperCase(),
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.API_TOKEN}`,
+              "content-type": "text/plain",
+            },
+          }
+        );
+      }
+      return "Done.";
+    } catch (err) {
+      return `Error: ${err.message}`;
+    }
+  },
+  async function update_shutters(action, ids) {
+    try {
+      for (const id of ids) {
+        await axios.post(
+          `${process.env.BASE_URL}/rest/items/${id}`,
+          action.toUpperCase(),
+          {
+            headers: {
+              Authorization: `Bearer ${process.env.API_TOKEN}`,
+              "content-type": "text/plain",
+            },
+          }
+        );
+      }
+      return action === "stop" ? "Done." : "In progress.";
+    } catch (err) {
+      console.log(err);
+      return `Error: ${err.message}`;
+    }
   },
 ]);
 
